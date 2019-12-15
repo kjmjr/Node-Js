d35c087aafeb0e77eabfa5d9a0b42df2ef100e86
Kevin McAdoo <53047422+kjmjr@users.noreply.github.com>
2019-12-15 00:16:22 -0500

    WooCommerce Worpress Template
    This project does not include all files because the project is to big but it is a brief intro into my work implementing Node JS to make a woo-commerce template for WordPress from scratch

---
+++ header.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<?php wp_head();?>
+
+</head>
+<body <?php body_class('test');?>>
+
+
+
+
+
+
+
+<header>
+	
+	<div class="container">
+		
+
+	</div>
+</header>
